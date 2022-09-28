68978a5d6fa21e397c18675c9c65a40623c7eb46
nammiju <nmj_1025@naver.com>
2022-09-28 21:23:53 +0900

    0922 숙제 해결은 못함ㅋ

--- P20220928/src/co/edu/variable/SortExe.java
+++ P20220928/src/co/edu/variable/SortExe.java
@@ -10,13 +10,25 @@ public class SortExe {
 		// 케이스 : n1 < n2 > n3
 		
 		if(numAry[0] > numAry[1]) { // 34 > 21
-			int temp = numAry[0]; // 빈 변수에 34를 먼저 담아줌 - null 21 12
+			int temp = numAry[0]; // 빈 변수에 34를 먼저 옮겨줌 - null 21 12
 			numAry[0] = numAry[1]; // 34자리 있었던 빈 자리에 21을 담아줌 - 21 null 12
-			numAry[1] = numAry[0]; // 
-			numAry[1] = temp;
+			numAry[1] = numAry[0]; // 2번째 자리에 34 넣어줌 - 21 34 12
+			numAry[1] = temp; // 다시 1번째 값을 빈공간에 담아줌
 		}
+//		if(numAry[0] > numAry[2]) {
+//			int temp = numAry[0];
+//			numAry[0] = numAry[2];
+//			numAry[2] = numAry[0];
+//		}
+//		if(numAry[0] > numAry[1]) { 
+//			int tem2= numAry[0]; 
+//			numAry[0] = numAry[1]; 
+//			numAry[1] = numAry[0];
+//		}
 		for(int n : numAry) {
 			System.out.println(n);
 		}
+		
+
 	}
 }
